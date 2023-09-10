7ab1fc5d5c768280d20dd05af7ad053623edbca7
kardo <karol.taikhoa@interia.eu>
2023-09-10 23:54:44 +0200

    imports for supporting pdf loading

--- file_processing/document_loader.py
+++ file_processing/document_loader.py
@@ -1,4 +1,22 @@
-from langchain.document_loaders import TextLoader
+from typing import List
 
-loader = TextLoader("./REAME.md")
-loader.load()
+from langchain.schema.document import Document
+from langchain.document_loaders import TextLoader
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import CharacterTextSplitter
+
+from preprocess_configs import *
+
+
+class DocumentLoader:
+    def preprocess_file(filename: str) -> List[Document]:
+        loader = TextLoader(KB_FILEPATH)
+        textsplitter = CharacterTextSplitter(
+            separator=separators, chunk_size=CHUNK_SIZE, chunk_overlap=CHUNK_OVERLAP
+        )
+        docs: List[Document] = loader.load_and_split(textsplitter)
+
+        return docs
+
+    def process_knowledge_base(kb_filepath: str = KB_FILEPATH) -> List[Document]:
+        pass
