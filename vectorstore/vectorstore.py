90a2d1b9f33a50206fb8afca84c26229ebc7e564
kardo <karol.taikhoa@interia.eu>
2023-09-13 16:40:02 +0200

    adding loading docs from disk feature

--- vectorstore/vectorstore.py
+++ vectorstore/vectorstore.py
@@ -1,3 +1,5 @@
+import os
+import sys
 from typing import List
 
 from langchain.schema.document import Document
@@ -6,18 +8,32 @@ from langchain.vectorstores import Chroma
 from langchain.vectorstores.base import VectorStore
 from langchain.vectorstores.base import VectorStoreRetriever
 
+from db_configs import DB_PATH
+
 
 class VectorDB:
     def __init__(self, chunked_docs: List[Document], embeddings: Embeddings):
         self.docs = chunked_docs
         self.embedding = embeddings
 
-    def create_chroma_vectorstore(self) -> VectorStore:
-        vectordb: VectorStore = Chroma.from_documents(
-            documents=self.docs, embedding=self.embedding
+    def create_chroma_vectorstore(
+        self, db_path: str = None, save_db_path: str = DB_PATH
+    ) -> VectorStore:
+        if db_path is None:
+            vectordb: VectorStore = Chroma.from_documents(
+                documents=self.docs, embedding=self.embedding, persist_directory=db_path
+            )
+            return vectordb
+
+        if not os.path.exists(db_path):
+            print(f"The path to chroma db - {db_path}, does not exist\n")
+            sys.exit(-2)
+
+        vectordb: VectorStore = Chroma(
+            persist_directory=db_path, embedding_function=self.embedding
         )
         return vectordb
 
     def get_db_retriever(self) -> VectorStoreRetriever:
-        vectordb = self.create_chroma_vectorstore()
+        vectordb: VectorStore = self.create_chroma_vectorstore()
         return vectordb.as_retriever()
